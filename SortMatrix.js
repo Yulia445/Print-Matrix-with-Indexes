b0e73803bfe5eecb51e18e824fa8feb0ce6d3ad2
Yulia Hoisan <yulia.v.goisan@ukd.edu.ua>
2025-10-05 22:27:12 +0300

    Create SortMatrix.js
    Sort MAtrix

---
+++ SortMatrix.js
@@ -0,0 +1,16 @@
+const numbers = [
+  [5,2,3],
+  [1,0,9],
+  [7,1,9],
+];
+
+for (let yulichka = 0; yulichka < numbers.length; yulichka++) {
+  for (let NumArr = 0; NumArr < numbers[yulichka].length; NumArr++) {
+
+
+
+    console.log(${yulichka + 1} ${NumArr + 1} = ${numbers[yulichka][NumArr]});
+
+
+  }
+}
